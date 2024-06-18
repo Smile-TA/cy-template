93190b84c5f4b8569a4f51c4ba0283d5bf4151a0
Smile-TA <smile.bestybay@thomasarts.com>
2024-06-18 14:03:18 -0700

    add og meta check

--- cypress/e2e/checkMeta.cy.js
+++ cypress/e2e/checkMeta.cy.js
@@ -4,7 +4,12 @@ describe("Check pages for meta", () => {
   pages.forEach((page) => {
     it(`on ${page} page`, () => {
       cy.visit(page);
-      cy.get('meta[name*="description"]');
+      cy.get('meta[name*="description"]')
+        .should("have.attr", "content")
+        .should("have.length.greaterThan", 0);
+      cy.get('meta[property*="og:description"]')
+        .should("have.attr", "content")
+        .should("have.length.greaterThan", 0);
     });
   });
 });
