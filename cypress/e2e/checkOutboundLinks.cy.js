ab1c483d5663d660a1e6d2289e2d0725609ea631
Smile-TA <smile.bestybay@thomasarts.com>
2024-06-25 23:21:14 -0700

    add outbound links target attribute test Need to look into soft assertions

---
+++ cypress/e2e/checkOutboundLinks.cy.js
@@ -0,0 +1,37 @@
+import pages from "../fixtures/pages.json";
+
+describe("Check outbound links open in a new tab", () => {
+  pages.forEach((page) => {
+    it(`on ${page} page`, () => {
+      cy.visit(page);
+      cy.get("a").then((links) => {
+        const filteredLinks = [];
+        for (const link of links) {
+          const href = link.getAttribute("href");
+          if (
+            !Boolean(href) ||
+            href === "#" ||
+            href === "/" ||
+            href.slice(0, 4) === "tel:" ||
+            href.slice(0, 7) === "mailto:" ||
+            getLinkBaseURL(href) === Cypress.config().baseUrl
+          ) {
+            continue;
+          }
+          filteredLinks.push(link);
+        }
+        filteredLinks.forEach((link) => {
+          cy.wrap(link).should("have.attr", "target").should("eq", "_blank");
+        });
+      });
+    });
+  });
+});
+
+const getLinkBaseURL = (href) => {
+  let pathArray = href.split("/");
+  let protocol = pathArray[0];
+  let host = pathArray[2];
+  let url = protocol + "//" + host + "/";
+  return url;
+};
